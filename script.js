1b1385dd421e7833c5de52cc3abf14892cefb843
Nagamanicoder <enagamani2605.com>
2026-01-30 16:28:58 +0530

    Completed

--- script.js
+++ script.js
@@ -1,113 +1,131 @@
-//element id's
-const expenseName = document.getElementById('expense-name');
-const expenseAmount = document.getElementById('expense-amount');
-const expenseCategory = document.getElementById('expense-category');
-const expenseDate = document.getElementById('expense-date')
 
-const totalAmount = document.getElementById('totalAmount')
-const expensesForm = document.getElementById('expensesForm');
-//submit button for action
-const submitButton = document.getElementById('submit');
-//expenses table 
-const expenseTable = document.getElementById('expense-table');
+   const totalAmount = document.getElementById('totalAmount')
+    const expensesForm = document.getElementById('expensesForm');
+    const submitButton = document.getElementById('submit');
+    const expenseTable = document.getElementById('expense-table');
 
-submitButton.addEventListener('click', function(e){
-    e.preventDefault();
-    validateData(expenseName,expenseAmount,expenseCategory,expenseDate);
-})
+    const filterCategory = document.getElementById('filter-category');
 
 
-function validateData(expenseName,expenseAmount,expenseCategory,expenseDate){
-    console.log({ expenseName, expenseAmount, expenseCategory, expenseDate });
+    let expenses = [];
+    let currentFilter = "All";
 
-    if(!expenseName.value.trim()  || /\d/.test(expenseName.value) || !expenseAmount.value.trim()
-        || expenseCategory === 'select category' || !expenseDate.value.trim())
-    {
-        alert('Please enter the expense details correctly!!')
-    }
-    else{
-        addExpenses(expenseName,expenseAmount,expenseCategory,expenseDate);
-    }
-}
+    submitButton.addEventListener('click', function(e){
+        e.preventDefault();
+        const expenseName = document.getElementById('expense-name').value;
+        const expenseAmount = parseFloat(document.getElementById('expense-amount').value);
+        const expenseCategory = document.getElementById('expense-category').value;
+        const expenseDate = document.getElementById('expense-date').value;
 
-
-function createActionCell() {
-    //edit and delete buttons
-    //edit button
-    const editButton = document.createElement('button')
-    editButton.className = 'edit-btn action-btn'
-    editButton.setAttribute('id', 'editButton')
-    editButton.textContent = 'Edit'
-
-    //delete button
-    const deleteButton = document.createElement('button')
-    deleteButton.className = 'delete-btn action-btn'
-    deleteButton.setAttribute('id', 'deleteButton')
-    deleteButton.textContent = 'Delete'
-
-    return {editButton, deleteButton};
-}
-
-//adding the expenses data into the table
-function addExpenses(...args){
-    const tableRow = document.createElement('tr')
-    const tableData = document.createElement('td')
-    for (let arg of args) {
-        console.log(arg);
-        
-        const tableData = document.createElement('td')
-        tableData.innerHTML = arg.value
-
-        if(arg.name === 'amount') {
-            calculateExpenses(parseInt(arg.value))
+        const expense = {
+            id: Date.now(), //the id helps to get the required table row
+            expenseName,
+            expenseAmount,
+            expenseCategory,
+            expenseDate
+            
         }
-        tableRow.appendChild(tableData);
+
+        
+        
+        if(validateData(expense)){
+            expenses.push(expense)
+            render();
+            expensesForm.reset();
+        }
+
+        
+    })
+
+
+    function validateData(obj){
+        if(!obj.expenseName.trim()  || /\d/.test(obj.expenseName) || !obj.expenseAmount
+        || obj.expenseCategory === 'select category' || !obj.expenseDate.trim())
+        {
+                alert('Please enter the expense details correctly!!')
+                return false;
+        }
+    
+        return true
+        
     }
-    const {editButton, deleteButton} = createActionCell()
-    tableData.append(editButton, deleteButton)
-    tableRow.appendChild(tableData)
-    expenseTable.appendChild(tableRow)
 
-    //resets the form or empties the form
-    expensesForm.reset() 
+    //adding the expenses data into the table
+    function addExpenses(expenses){
+        expenseTable.innerHTML = ""; //this makes only at the frontend the values are appended to the table body but not in the backend
+        expenses.forEach(expense => {
+            const tableRow = document.createElement('tr');
 
+            tableRow.innerHTML = 
+                `
+                    <td> ${expense.expenseName} </td>
+                    <td> ${expense.expenseAmount} </td>
+                    <td> ${expense.expenseCategory} </td>
+                    <td> ${expense.expenseDate} </td>
+                    <td> 
+                        <button class="edit-btn" id=${expense.id}>Edit</button> 
+                        <button class="delete-btn" id=${expense.id}>Delete</button> 
+                    </td>
+                `
+                expenseTable.appendChild(tableRow)
+                
+        });
+    }
 
-    //editing the expenses details row
-    editButton.addEventListener('click', function(){
-        editExpensesDetails(tableRow)
+    //calculating total expense amount
+    function calculateExpensesTotal() {
+        const total = expenses.reduce((initialValue, expense) => initialValue + expense.expenseAmount, 0);
+        totalAmount.textContent = total ? `${total} Rupees` : `${total} Rupee`
         
+    }
+
+
+    expenseTable.addEventListener('click', function(e){
+
+        if(e.target.classList.contains("delete-btn")){
+            const removeId = parseInt(e.target.id)
+            expenses = expenses.filter(expense => expense.id !== removeId);
+            
+            render()
+        }
+
+        if(e.target.classList.contains("edit-btn")){
+            const removeId = parseInt(e.target.id)
+            const expense = expenses.find( (expense) => expense.id === removeId )
+
+            document.getElementById('expense-name').value = expense.expenseName
+            document.getElementById('expense-amount').value = expense.expenseAmount
+            document.getElementById('expense-category').value = expense.expenseCategory
+            document.getElementById('expense-date').value = expense.expenseDate
+
+            expenses = expenses.filter(expense => expense.id !== removeId);
+
+            render()
+            
+        }
     })
 
-    //deleting the expenses details row
-    deleteButton.addEventListener('click', function(){
-        deleteExpensesDetails(tableRow);
-        
-    })
+
+    filterCategory.addEventListener("change", (e) => {
+        currentFilter = e.target.value;
+        render();
+    });
+
+
+function render(){
+    let data = expenses;
+
+    if(currentFilter !== "All"){
+        data = expenses.filter(
+            e => e.expenseCategory.trim() === currentFilter.trim()
+        );
+    }
+
+    addExpenses(data);
+    calculateExpensesTotal();
 }
 
-//calculating total expense amount
-function calculateExpenses(amount) {
-    let current = parseInt(totalAmount.textContent);
-    let updated = current + amount;
-    totalAmount.textContent = `${updated} Rupees`
-}
 
-// editing the expenses details and save need to do
-function editExpensesDetails(tableRow){
-    
-    console.log(tableRow);
-    
-    // addExpenses(cells) 
-}
-
-//deleting the expenses details
-function deleteExpensesDetails(tableRow){
-    tableRow.remove()
-    //minus the amount
-}
-
-//filter the total amount based on the category
-//persistent storage -> websocket, local storage
 
 
 
